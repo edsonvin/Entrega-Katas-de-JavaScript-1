0b39bba8de1790d577de8afa8810bd45a7fde48b
edsonvin <91762080+edsonvin@users.noreply.github.com>
2021-10-15 21:23:49 -0300

    Add files via upload

---
+++ noturno-entrega-s1-katas-javascript-1-edsonvin/src/katas1.js
@@ -0,0 +1,119 @@
+
+//function oneThroughTwenty
+function oneThroughTwenty() {
+  
+  for(let contador=1;contador<=20;contador++){
+    console.log(contador)
+  }
+}
+ oneThroughTwenty()
+  
+  
+
+
+
+// function evensToTwenty() 
+function evensToTwenty() {
+  let counter = 1
+  while (counter<=20){
+         if(counter % 2 === 0){
+      console.log(counter)   
+      }
+      counter++}
+}
+ evensToTwenty()
+
+// function oddsToTwenty() 
+   
+function oddsToTwenty() {
+  let cont = 1
+  while(cont<=20){
+    if(cont % 2 != 0){
+      console.log(cont)
+    }
+    cont++
+  }
+
+}
+oddsToTwenty()
+
+
+// function multiplesOfFive()
+
+function multiplesOfFive() {
+  let contM5 = 1
+  while(contM5<=100){
+    if(contM5 % 5 === 0){
+      console.log(contM5)
+    }
+    contM5++
+  }
+
+}
+multiplesOfFive()
+
+
+// function squareNumbers() 
+  function squareNumbers(){
+  for(let contador = 1; contador <=10; contador++)
+  console.log((contador)**2)}
+  squareNumbers()
+
+// //call function squareNumbers
+
+// function countingBackwards() 
+    
+function countingBackwards(){
+  for(count = 20; count>=1; count--){
+    console.log(count)
+  }
+}
+countingBackwards()
+
+
+
+// function evenNumbersBackwards() {
+  function evenNumbersBackwards() {
+      let counterEB = 20
+      while (counterEB>=1){
+             if(counterEB % 2 === 0){
+          console.log(counterEB)   
+          }
+          counterEB--}
+    }
+    evenNumbersBackwards()
+    
+
+// oddNumbersBackwards()
+    function oddNumbersBackwards(){
+      contOB = 20
+      while(contOB>0){
+        if(contOB % 2 != 0){
+          console.log(contOB)
+        }
+        contOB--
+      }
+    }
+    oddNumbersBackwards()
+
+
+// function multiplesOfFiveBackwards() {
+    function multiplesOfFiveBackwards(){
+      let contM5B = 100
+      while(contM5B>0){
+        if(contM5B % 5 === 0){
+          console.log(contM5B)
+        }
+        contM5B--
+      }
+    }
+    multiplesOfFiveBackwards()
+
+// function squareNumbersBackwards() {
+    function squareNumbersBackwards(){
+      for(let contSB = 10; contSB>0; contSB--){
+        console.log(contSB**2)
+      }
+    }
+    squareNumbersBackwards()
+
